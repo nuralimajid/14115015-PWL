4340ab46ea3c59199d201d031ede06d452018d06
Majidil Khithar <nuralimajids@gmail.com>
2020-04-18 00:10:02 +0700

    wew

--- Praktikum4/shop/src/index.js
+++ Praktikum4/shop/src/index.js
@@ -2,12 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Blog from './pages/Blog';
+import profile from './pages/profile';
+import baca from './pages/baca';
 import * as serviceWorker from './serviceWorker';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/profile">profile</Link>
+        </li>
+        <li>
+          <Link to="/Blog">Blog</Link>
+        </li>
+      </ul>
+      <Route path="/" component={App} />
+      <Route exact path="pages/profile" component={profile} />
+      <Route exact path="/Blog" component={Blog} />
+      <Route path="/Blog/:id" component={baca} />
+
+    </div>
+  </Router>
+  // 
+  ,
   document.getElementById('root')
 );
 
